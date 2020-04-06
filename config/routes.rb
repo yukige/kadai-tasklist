ba5b0318105481ff7b3a4060c8f2b33cc65f6fd5
taro.kirameki <taro.kirameki@techacademy.jp>
2020-04-06 16:27:20 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
@@ -8,6 +11,10 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
 
   resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
